586f8b9f68cbd36553e959d9c8d6b2d90b55afe1
Rasha ali <rashaali84@gmail.com>
2020-02-02 19:23:40 +0100

    testing and modify reading methods

--- scripts/listener.js
+++ scripts/listener.js
@@ -1,15 +1,22 @@
-function calc(a, b, op) {
-  var regex = /^[a-zA-Z]+$/;
-  if (a.match(regex) || b.match(regex)) {
-    document.querySelector("#results").innerText = "Please write numbers only";
-  } else if (a.length < 1 || b.length < 1) {
-    document.querySelector("#results").innerText = "please fill in text-boxes";
-  } else {
-    calculateHandler(a, b, op);
-    clear();
-  }
+document.getElementById("btnOperation").addEventListener("click", calc);
+
+function calc() {
+    var mForm = document.getElementById('mainForm');
+    var a = mForm['value1'].value;
+    var b = mForm['value2'].value;
+    var op = mForm['operator'].value;
+
+    var regex = /^[a-zA-Z]+$/;
+    if (a.match(regex) || b.match(regex)) {
+        document.querySelector("#results").innerText = "Please write numbers only";
+    } else if (a.length < 1 || b.length < 1) {
+        document.querySelector("#results").innerText = "please fill in text-boxes";
+    } else {
+        calculateHandler(a, b, op);
+        clear();
+    }
 }
 function clear(event) {
-  document.getElementById("value1").value = "";
-  document.getElementById("value2").value = "";
+    document.getElementById("value1").value = "";
+    document.getElementById("value2").value = "";
 }
