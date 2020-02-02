29b63e39022d8f436988d155881a722bd8414a47
Rasha ali <rashaali84@gmail.com>
2020-02-02 18:03:52 +0100

    add handler and process user inputs

--- scripts/handler.js
+++ scripts/handler.js
@@ -1,12 +1,17 @@
-function calculateHandler(event) {
+function calculateHandler(a, b, op) {
   // read & process user input from event
-
+  a = parseInt(a);
+  b = parseInt(b);
+  op = op.toString();
   // pass user input through core logic
 
+  var calculate;
+  calculate = doMath(op, a, b);
   // render output to DOM for user
-
+  document.querySelector("#results").innerText =
+    a + " " + op + " " + b + " " + " = " + calculate;
   // log user action for developers
-
+  console.log(calculate);
   // return true for the browser
   return true;
 }
